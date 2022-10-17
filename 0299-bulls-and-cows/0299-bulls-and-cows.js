37c04d5684f8f7b7c1356e62deafa8de25ae0ca8
Dike Goodluck <gudoski30@gmail.com>
2022-10-17 21:23:48 +0100

    Time: 102 ms (77.41%), Space: 43 MB (96.97%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} secret
+ * @param {string} guess
+ * @return {string}
+ */
+function getHint(secret, guess) {
+    let cows = 0;
+    let bulls = 0;
+    const occurrences = Array(10).fill(0);
+
+    for (let i = 0; i < secret.length; i++) {
+        const guessNumber = parseInt(guess[i]);
+        const secretNumber = parseInt(secret[i]);
+        
+        if (secretNumber === guessNumber) {
+            bulls++;
+            continue
+        }
+
+        if(occurrences[secretNumber] < 0) {
+            cows++;
+        }
+        
+        if(occurrences[guessNumber] > 0) {
+            cows++;
+        }
+        
+        occurrences[secretNumber]++;
+        occurrences[guessNumber]--;
+    }
+  
+    return `${bulls}A${cows}B`;
+}
